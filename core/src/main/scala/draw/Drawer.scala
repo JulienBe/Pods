a74eb69ec9fafababcf23bb93e08a207fc05dfc0
JulienBe <bertozzijulien@gmail.com>
2016-07-29 21:42:31 +0200

    Drawing

---
+++ core/src/main/scala/draw/Drawer.scala
@@ -0,0 +1,21 @@
+package draw
+
+import brols.Timer
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+
+class Drawer(gdxProvider: GdxProvider) extends Screener(gdxProvider) with GdxProvider {
+
+  val timer = new Timer(90)
+  var finished = false
+
+  override def render(delta: Float) = {
+    camera.update()
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+
+    if (timer.step(delta))
+      Gdx.app.log("DEBUG", "Finished :)")
+  }
+
+}
---
+++ core/src/main/scala/draw/GdxProvider.scala
@@ -0,0 +1,19 @@
+package draw
+
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import main.Rome
+
+/**
+  * Created by julein on 05/07/16.
+  */
+trait GdxProvider {
+  def getCamera() = {
+    val cam = new OrthographicCamera(Rome.world.width, Rome.world.height)
+    cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0)
+    cam
+  }
+  def getShapeRenderer() = new ShapeRenderer()
+  def getSpriteBatch() = new SpriteBatch()
+}
